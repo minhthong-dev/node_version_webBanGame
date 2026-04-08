6084ed4872dc0a18d23fef9c1862547d74ec0635
ssamkkura <tuantuntun03@gmail.com>
2026-04-08 20:24:47 +0700

    fix:checkout wallet

--- controllers/buyController.js
+++ controllers/buyController.js
@@ -14,4 +14,19 @@ const buyGame = async (req, res) => {
     }
 };
 
-module.exports = { buyGame };
+const buyWallet = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { walletId } = req.body;
+        const result = await buyService.buyWallet(userId, walletId);
+        if (result.error) {
+            const { error, ...rest } = result;
+            return res.status(400).json({ error, ...rest });
+        }
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi hệ thống' });
+    }
+}
+
+module.exports = { buyGame, buyWallet };
--- routes/buy.js
+++ routes/buy.js
@@ -4,6 +4,6 @@ const buyController = require('../controllers/buyController');
 const validateTokenExpires = require('../middlewares/validateTokenExpires');
 
 router.post('/', validateTokenExpires, buyController.buyGame);
-router.post('/wallet', validateTokenExpires, buyController.buyGame);
+router.post('/wallet', validateTokenExpires, buyController.buyWallet);
 
 module.exports = router;
